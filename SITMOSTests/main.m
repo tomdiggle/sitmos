9d0afa35087929c2e6c720268415c484a3de39e5
Tom Diggle <tom@idlegeniussw.com>
2013-01-21 19:15:57 +0000

    Change unit tests are now conducted using GHUnit
    GHUnit has been added as a git submodule located in Vendor folder. The IGMediaPlayerTests have been change to support GHUnit.

---
+++ SITMOSTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SITMOSTests
+//
+//  Created by Tom Diggle on 21/01/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
