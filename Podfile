20a040be04d8bedc4fe8b6f9a1dd1d1727109128
Tom Diggle <tom@idlegeniussw.com>
2013-06-10 15:16:35 +0100

    Update all the pods

--- Podfile
+++ Podfile
@@ -1,10 +1,10 @@
 platform :ios, '6.0'
 
-pod 'AFNetworking', '~> 1.2.1'
+pod 'AFNetworking', '~> 1.3.0'
 pod 'AFDownloadRequestOperation', '~> 0.0.2'
 pod 'MagicalRecord', '~> 2.1'
-pod 'TestFlightSDK', '~> 1.3.0.beta2'
+pod 'TestFlightSDK', '~> 1.3.0-beta.5'
 
 target :SITMOSTests, :exclusive => true do
-    pod 'OCHamcrest', '~> 2.0.0'
+    pod 'OCHamcrest', '~> 2.0.1'
 end
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFDownloadRequestOperation
+#define COCOAPODS_POD_AVAILABLE_AFDownloadRequestOperation
+#define COCOAPODS_VERSION_MAJOR_AFDownloadRequestOperation 0
+#define COCOAPODS_VERSION_MINOR_AFDownloadRequestOperation 0
+#define COCOAPODS_VERSION_PATCH_AFDownloadRequestOperation 2
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// MagicalRecord
+#define COCOAPODS_POD_AVAILABLE_MagicalRecord
+#define COCOAPODS_VERSION_MAJOR_MagicalRecord 2
+#define COCOAPODS_VERSION_MINOR_MagicalRecord 1
+#define COCOAPODS_VERSION_PATCH_MagicalRecord 0
+
+// TestFlightSDK
+#define COCOAPODS_POD_AVAILABLE_TestFlightSDK
+#define COCOAPODS_VERSION_MAJOR_TestFlightSDK 1
+#define COCOAPODS_VERSION_MINOR_TestFlightSDK 3
+#define COCOAPODS_VERSION_PATCH_TestFlightSDK 0
+
